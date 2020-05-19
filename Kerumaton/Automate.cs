c38dbbda201acde9ac95fbe94bae81b5c30d2aa3
ker2x <kerdezixe@gmail.com>
2020-05-19 16:48:14 +0200

    Ajoutez des fichiers projet.

---
+++ Kerumaton/Automate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace Kerumaton
+{
+    class Automate
+    {
+        // The automate position
+        public class Position
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+
+            public Position()
+            {
+                x = 0;
+                y = 0;
+            }
+
+            public Position(int _x, int _y)
+            {
+                x = _x;
+                y = _y;
+            }
+
+            public override string ToString() => $"({x}, {y})";
+        }
+
+        public Position pos;
+
+        public enum Direction { N,NE,E,SE,S,SW,W,NW}
+        public int id { get; set; }
+        public int hp { get; set; }
+        public bool isAlive { get; set; }
+        public Colors color { get; set; }
+        public int energy { get; set; }
+        public int lifetime { get; set; }
+
+
+
+        public Automate(int x, int y, int id)
+        {
+            pos = new Position(x, y);
+            this.id = id;
+        }
+
+        public bool Move(Direction dir)
+        {
+            switch (dir)
+            {
+                case Direction.N:
+                    {
+                        if (pos.y == 0) return false;
+                        else
+                        {
+                            pos.y--;
+                            return true;
+                        }
+                    }
+                case Direction.S:
+                    {
+                        if (pos.y == MainWindow.bmp.PixelHeight - 1) return false;
+                        else
+                        {
+                            pos.y++;
+                            return true;
+                        }
+                    }
+                default:
+                    return false;
+            }
+        }
+
+    }
+}
---
+++ Kerumaton/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;     //WriteableBitmap
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.Concurrent;    //BlockingCollection<T>
+using System.Diagnostics;               // stopwatch
+
+
+namespace Kerumaton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public static WriteableBitmap bmp;
+        Random rand;
+        Stopwatch stopwatch;
+        List<Automate> bots;
+
+        public static int imageWidth = 80;
+        public static int imageHeight = 60;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            rand = new Random();
+            bmp = new WriteableBitmap(imageWidth, imageHeight, 96, 96, PixelFormats.Bgr32, null);
+            bots = new List<Automate>();
+
+            //Sample bot
+            bots.Add(new Automate(10, 58, 1));
+            bots.Add(new Automate(12, 58, 2));
+
+            //Set xaml Image as the bmp & update the screen
+            MainImage.Source = bmp;
+            ScreenUpdate();
+            bots.Add(new Automate(14, 58, 3));
+            ScreenUpdate();
+        }
+
+        private void ScreenUpdate()
+        {
+            bmp.Lock();
+            bmp.Clear(Colors.Black);
+            foreach (var b in bots) bmp.SetPixel(b.pos.x, b.pos.y, Colors.Red);
+            bmp.AddDirtyRect(new Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight));
+            bmp.Unlock();
+        }
+
+        private void MainImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+        }
+    }
+}
